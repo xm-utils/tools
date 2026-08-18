e6d212132aca9c34b648ea5e35aa552e88c86a45
zhangyuan <zhangyuan@novgate.com>
2026-08-18 19:05:13 +0800

    common

--- common/go.mod
+++ common/go.mod
@@ -4,8 +4,11 @@ go 1.24.2
 
 require (
 	github.com/gin-gonic/gin v1.10.0
+	github.com/go-playground/validator/v10 v10.20.0
 	github.com/golang-jwt/jwt/v4 v4.5.2
 	github.com/google/uuid v1.6.0
+	github.com/shopspring/decimal v1.4.0
+	github.com/sirupsen/logrus v1.9.4
 	golang.org/x/crypto v0.23.0
 )
 
@@ -18,7 +21,6 @@ require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
@@ -27,7 +29,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
-	github.com/sirupsen/logrus v1.9.4 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
--- common/math.go
+++ common/math.go
@@ -1,9 +1,9 @@
 package common
 
 import (
-	"fmt"
-	"math"
 	"strconv"
+
+	"github.com/shopspring/decimal"
 )
 
 // AbsInt 对int类型取绝对值
@@ -16,55 +16,42 @@ func AbsInt(num int64) int64 {
 
 // Cent2Yuan 人民币分转为元
 func Cent2Yuan(fen uint64) float64 {
-	yuan := float64(fen) / 100.0
-	return yuan
+	return decimal.NewFromUint64(fen).Div(decimal.NewFromInt(100)).InexactFloat64()
 }
 func Cent2YuanStr(fen uint64) string {
-	yuan := float64(fen) / 100.0
-	return fmt.Sprintf("%.2f", yuan)
+	return decimal.NewFromUint64(fen).Div(decimal.NewFromInt(100)).StringFixed(2)
 }
 
 func YuanStr2Cent(str string) uint64 {
-	yuan := StringToFloat64(str)
-	return Yuan2Cent(yuan)
+	yuan, _ := decimal.NewFromString(str)
+	return Yuan2Cent(yuan.InexactFloat64())
 }
 
 // Yuan2Cent 人民币元转为分
 func Yuan2Cent(yuan float64) uint64 {
-	return uint64(yuan * 100)
-	//de := decimal.NewFromFloat(yuan)
-	//return de.Mul(decimal.NewFromFloat(100)).IntPart()
+	return uint64(decimal.NewFromFloat(yuan).Mul(decimal.NewFromInt(100)).IntPart())
 }
 
 // RateToClient 比率还原后返回客户端
 func RateToClient(num uint64) float64 {
-	rate := float64(num) / float64(10000)
-	return rate
+	return decimal.NewFromUint64(num).Div(decimal.NewFromInt(10000)).InexactFloat64()
 }
 func Rate2ClientStr(num uint64) string {
-	rate := float64(num) / float64(10000)
-	return fmt.Sprintf("%.2f", rate)
+	return decimal.NewFromUint64(num).Div(decimal.NewFromInt(10000)).StringFixed(2)
 }
 
 func Rate2DB(yuan float64) uint64 {
-	return uint64(yuan * 10000)
-	//de := decimal.NewFromFloat(yuan)
-	//return de.Mul(decimal.NewFromFloat(10000)).IntPart()
+	return uint64(decimal.NewFromFloat(yuan).Mul(decimal.NewFromInt(10000)).IntPart())
 }
 
 func RateStr2DB(str string) uint64 {
-	yuan := StringToFloat64(str)
-	return uint64(yuan * 10000)
-	//de := decimal.NewFromFloat(yuan)
-	//return de.Mul(decimal.NewFromFloat(10000)).IntPart()
+	yuan, _ := decimal.NewFromString(str)
+	return uint64(yuan.Mul(decimal.NewFromInt(10000)).IntPart())
 }
 
 func StringToFloat64(str string) float64 {
-	value, err := strconv.ParseFloat(str, 32)
-	if err != nil {
-		return 0
-	}
-	return value
+	yuan, _ := decimal.NewFromString(str)
+	return yuan.InexactFloat64()
 }
 
 func StringToInt32(s string) int32 {
@@ -76,11 +63,11 @@ func StringToInt32(s string) int32 {
 }
 
 func StringToInt64(s string) int64 {
-	i, err := strconv.ParseInt(s, 10, 64)
-	if err != nil {
+	d, err2 := decimal.NewFromString(s)
+	if err2 != nil {
 		return 0
 	}
-	return i
+	return d.IntPart()
 }
 
 func StringToUInt(s string) uint {
@@ -100,27 +87,13 @@ func StringToUInt64(s string) uint64 {
 }
 
 func StringToInt(s string) int {
-	num, err := strconv.Atoi(s)
-	if err != nil {
+	d, err2 := decimal.NewFromString(s)
+	if err2 != nil {
 		return 0
 	}
-	return num
+	return int(d.IntPart())
 }
 
 func Float64ToString(value float64) string {
-
-	return strconv.FormatFloat(value, 'f', 2, 64)
-}
-
-func TuiGuangAmountConversion(str string) string {
-	floatValue, err := strconv.ParseFloat(str, 32)
-	if err != nil {
-		return ""
-	}
-	return strconv.Itoa(int(floatValue * 100))
-}
-
-// 浮点数向下取整
-func Float32Floor(float float32) float32 {
-	return float32(math.Floor(float64(float)))
+	return decimal.NewFromFloat(value).StringFixed(2)
 }

54cdd16319c100427169a365e72a42cf28acf05b
zhangyuan <zhangyuan@novgate.com>
2026-08-25 15:43:56 +0800

    common 随机字符串

--- common/StrUtil.go
+++ common/StrUtil.go
@@ -232,22 +232,23 @@ func ParseStrToArrayStr(target string, sep string) []string {
 }
 
 // RandomString 在数字、大写字母、小写字母范围内生成num位的随机字符串
+// 一共62个字符，在0~61进行随机，小于10时，在数字范围随机，
+// 小于36在大写范围内随机，其他在小写范围随机
+// 48 ~ 57 数字
+// 65 ~ 90 A ~ Z
+// 97 ~ 122 a ~ z
 func RandomString(length int) string {
-	// 48 ~ 57 数字
-	// 65 ~ 90 A ~ Z
-	// 97 ~ 122 a ~ z
-	// 一共62个字符，在0~61进行随机，小于10时，在数字范围随机，
-	// 小于36在大写范围内随机，其他在小写范围随机
-	rand.Seed(time.Now().UnixNano())
 	result := make([]string, 0, length)
 	for i := 0; i < length; i++ {
 		t := rand.Intn(62)
 		if t < 10 {
 			result = append(result, strconv.Itoa(rand.Intn(10)))
 		} else if t < 36 {
-			result = append(result, strconv.Itoa(rand.Intn(26)+65))
+			intn := rand.Intn(26)
+			result = append(result, fmt.Sprintf("%c", 'A'+intn))
 		} else {
-			result = append(result, strconv.Itoa(rand.Intn(26)+97))
+			intn := rand.Intn(26)
+			result = append(result, fmt.Sprintf("%c", 'a'+intn))
 		}
 	}
 	return strings.Join(result, "")
